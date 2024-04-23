7663fa3a414157bd7ea3b6dc4dd6d71e83a5d888
lwilbur <42094235+lwilbur@users.noreply.github.com>
2024-04-23 10:45:27 -0400

    Update headTailMaker.sh to decrease verbosity

--- test_set/headTailMaker.sh
+++ test_set/headTailMaker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# NOTE: head -q does not exist on Mac OS -- use linux
 
 # Lengths
 len_90_perc=413
@@ -7,6 +8,12 @@ len_1pt25x100=625
 len_1pt5x100=750
 len_1pt75x100=875
 len_2x100=1000
+len=($len_90_perc\
+     $len_100_perc\
+     $len_1pt25x100\
+     $len_1pt5x100\
+     $len_1pt75x100\
+     $len_2x100)
 
 # Create directories
 dir_90_perc="mw_uzip_90_perc"
@@ -15,40 +22,29 @@ dir_1pt25x100_perc="mw_uzip_1pt25x100_perc"
 dir_1pt5x100_perc="mw_uzip_1pt5x100_perc"
 dir_1pt75x100_perc="mw_uzip_1pt75x100_perc"
 dir_2x100_perc="mw_uzip_2x100_perc"
+dir=($dir_90_perc\
+     $dir_100_perc\
+     $dir_1pt25x100_perc\
+     $dir_1pt5x100_perc\
+     $dir_1pt75x100_perc\
+     $dir_2x100_perc)
 
-mkdir "${dir_90_perc}"
-mkdir "${dir_100_perc}"
-mkdir "${dir_1pt25x100_perc}"
-mkdir "${dir_1pt5x100_perc}"
-mkdir "${dir_1pt75x100_perc}"
-mkdir "${dir_2x100_perc}"
+for d in ${dir[@]}; do
+    mkdir ${d}
+done
 
-# create head+tail file of each length
+# Create labels to append to filenames once head/tail removed
+append=("_90_perc" "_100_perc" "_1pt25" "_1pt5" "_1pt75" "_2")
+
+# For each file, create a head+tail file of each length
 for filename in malware_unzipped/*; do
     basename="${filename##*/}"
 
-    outfile="${dir_90_perc}/${basename}_90_perc"
-    echo "head -q --bytes "$len_90_perc" $filename > "$outfile""
-    echo "tail -q --bytes "$len_90_perc" $filename >> "$outfile""
-
-    outfile="${dir_100_perc}/${basename}_100_perc"
-    echo "head -q --bytes "$len_100_perc" $filename > "$outfile""
-    echo "tail -q --bytes "$len_100_perc" $filename >> "$outfile""
-
-
-    outfile="${dir_1pt25x100_perc}/${basename}_1pt25"
-    echo "head -q --bytes "$len_1pt25x100" $filename > "$outfile""
-    echo "tail -q --bytes "$len_1pt25x100" $filename >> "$outfile""
-
-    outfile="${dir_1pt5x100_perc}/${basename}_1pt5"
-    echo "head -q --bytes "$len_1pt5x100" $filename > "$outfile""
-    echo "tail -q --bytes "$len_1pt5x100" $filename >> "$outfile""
-
-    outfile="${dir_1pt75x100_perc}/${basename}_1pt75"
-    echo "head -q --bytes "$len_1pt75x100" $filename > "$outfile""
-    echo "tail -q --bytes "$len_1pt75x100" $filename >> "$outfile""
-
-    outfile="${dir_2x100_perc}/${basename}_2"
-    echo "head -q --bytes "$len_2x100" $filename > "$outfile""
-    echo "tail -q --bytes "$len_2x100" $filename >> "$outfile""
+    # Loop over indices in dir, creating each
+    for i in ${!dir[@]}; do
+        outfile="${dir[$i]}/${basename}${append[$i]}"
+        head -q --bytes "${len[$i]}" $filename > $outfile
+        tail -q --bytes "${len[$i]}" $filename >> $outfile
+    done
 done
+
